8fa0197e44fa407bb05a8784fd8e716be9400cca
dsidorin <sidorin.d@yandex.ru>
2022-02-09 17:05:45 +0300

    start of project

---
+++ cmake-build-debug/CMakeFiles/WhiteBeltFinal.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/WhiteBeltFinal.dir/main.cpp.o"
+  "WhiteBeltFinal"
+  "WhiteBeltFinal.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/WhiteBeltFinal.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/WhiteBeltFinal.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for WhiteBeltFinal.
+# This may be replaced when dependencies are built.
